3fe55c5a570fdc0eb101f6506436be65e41f8b34
Suliman Tekalli <71199733+sulisulisulwan@users.noreply.github.com>
2021-07-01 15:47:20 -0400

    Update db.js

--- products/db/db.js
+++ products/db/db.js
@@ -1,10 +1,12 @@
-const mysql = require('mysql')
+const mysql = require('mysql');
+const config = require('configDb.js');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Mis4mancy!',
-  database: 'products'
+  user: config.user,
+  password: config.password,
+  host: config.host,
+  port: config.port,
+  database: config.database
 });
 
 connection.connect((err) => {
